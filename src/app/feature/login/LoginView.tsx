705e23e37e96effc917433589311d66bae8c45d9
Renan Sigolo <renan.sigolo@hellofresh.com.au>
2022-01-10 13:53:20 +1100

    style: adjust components

--- src/app/feature/login/LoginView.tsx
+++ src/app/feature/login/LoginView.tsx
@@ -3,9 +3,6 @@ import { Link } from "react-router-dom";
 export function LoginView() {
   return (
     <div>
-      <br />
-      <br />
-      <br />
       <h1>Login Route</h1>
       <Link to="/">Go home</Link>
     </div>
--- src/app/feature/not-found/NotFound.tsx
+++ src/app/feature/not-found/NotFound.tsx
@@ -3,9 +3,6 @@ import { Link } from "react-router-dom";
 export function NotFound() {
   return (
     <div>
-      <br />
-      <br />
-      <br />
       <h1>Not Found</h1>
       <Link to="/">Go to home</Link>
     </div>
