09fb9d8f6594b4739e312bb4405c521715e85885
Renan Sigolo <renan.sigolo@hellofresh.com.au>
2022-02-24 21:46:44 +1100

    last test

--- scripts/check-semver-ci.js
+++ scripts/check-semver-ci.js
@@ -5,20 +5,23 @@ const semver = require("semver");
 const dsPackageJsonPath = path.resolve(__dirname, "..", "package.json");
 const dsPackageJson = JSON.parse(fs.readFileSync(dsPackageJsonPath));
 
+const oldVersion = dsPackageJson.version;
+const nextVersion = process.argv[2];
+
 (async () => {
   try {
-    const nextVersion = process.argv[2];
-
     // Check for a valid semver
     if (!nextVersion || !semver.valid(nextVersion))
-      throw new Error(`❌ Looks like this is not a valid semver`);
+      throw new Error("❌ Looks like this is not a valid semver");
 
-    if (semver.compareIdentifiers(nextVersion, dsPackageJson.version) !== 1)
-      throw new Error(`❌ The new version must be higher than the current version`);
+    if (semver.compareIdentifiers(nextVersion, oldVersion) !== 1)
+      throw new Error(
+        "❌ The new version must be higher than the current version"
+      );
 
     // Return a message to the console
     console.info(
-      `✅ The update from ${dsPackageJson.version} to ${nextVersion} is valid!`
+      `✅ The update from v${oldVersion} to v${nextVersion} is valid!`
     );
   } catch (error) {
     setImmediate(() => {
