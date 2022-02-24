22562f96ed3bfc52c376f18e5362e2475c4d1c62
Renan Sigolo <renan.sigolo@hellofresh.com.au>
2022-02-24 21:41:01 +1100

    add one more check

--- scripts/check-semver-ci.js
+++ scripts/check-semver-ci.js
@@ -1,25 +1,28 @@
-const fs = require('fs');
-const path = require('path');
-const semver = require('semver');
+const fs = require("fs");
+const path = require("path");
+const semver = require("semver");
 
-const dsPackageJsonPath = path.resolve(__dirname, '..', 'package.json');
+const dsPackageJsonPath = path.resolve(__dirname, "..", "package.json");
 const dsPackageJson = JSON.parse(fs.readFileSync(dsPackageJsonPath));
 
 (async () => {
-    try {
-        // Check for a valid semver
-        const nextVersion = process.argv[2];
-        if (!semver.valid(nextVersion) || !nextVersion)
-            throw new Error(`❌ Looks like this is not a valid semver`);
+  try {
+    const nextVersion = process.argv[2];
 
-        // Return a message to the console
-        console.info(`
-            ✅ The update from ${dsPackageJson.version} to ${nextVersion} is valid!
-            Proceding with the release...
-        `);
-    } catch (error) {
-        setImmediate(() => {
-            throw error;
-        });
-    }
+    // Check for a valid semver
+    if (!nextVersion || !semver.valid(nextVersion))
+      throw new Error(`❌ Looks like this is not a valid semver`);
+
+    if (semver.compareIdentifiers(nextVersion, dsPackageJson.version) !== 1)
+      throw new Error(`❌ The new version must be higher than the current version`);
+
+    // Return a message to the console
+    console.info(
+      `✅ The update from ${dsPackageJson.version} to ${nextVersion} is valid!`
+    );
+  } catch (error) {
+    setImmediate(() => {
+      throw error;
+    });
+  }
 })();
